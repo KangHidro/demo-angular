388e5038ca24a872589c48b4f4107f6540777ac3
duyll <duyll@hcmute.edu.vn>
2022-05-20 20:38:08 +0700

    a/c

--- src/app/vd06-custom-directives/number-separator.directive.ts
+++ src/app/vd06-custom-directives/number-separator.directive.ts
@@ -6,7 +6,7 @@ import { DecimalPipe } from '@angular/common';
   providers: [DecimalPipe]
 })
 export class NumberSeparatorDirective implements OnInit {
-  currencyChars = new RegExp('[.,]', 'g'); // we're going to remove commas and dots
+  chars = new RegExp('[.,]', 'g'); // we're going to remove commas and dots
 
   @HostListener('input', ['$event.target.value']) onInput(e: string) {
     this.format(e);
@@ -18,18 +18,16 @@ export class NumberSeparatorDirective implements OnInit {
       this.format(event.clipboardData.getData('text/plain'));
     }
   }
+
   constructor(private elSvc: ElementRef, private rendererSvc: Renderer2, private decimalPipeSvc: DecimalPipe) { }
 
   ngOnInit() {
     this.format(this.elSvc.nativeElement.value); // format any initial values
   }
 
-
-
   format(val: string) {
     // 1. test for non-number characters and replace/remove them
-    const numberFormat = parseInt(String(val).replace(this.currencyChars, ''), 10);
-    // console.log(numberFormat); // raw number
+    const numberFormat = parseInt(String(val).replace(this.chars, ''), 10);
 
     // 2. format the number (add dot)
     const n = this.decimalPipeSvc.transform(numberFormat, '1.0', 'vi-VN');
